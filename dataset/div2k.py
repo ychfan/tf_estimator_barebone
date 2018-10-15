bc79d64934373b085e342ad98ba2c83bf87a41e2
Yuchen Fan <fyc0624@gmail.com>
2018-10-14 23:20:50 -0500

    update edsr

--- dataset/div2k.py
+++ dataset/div2k.py
@@ -6,7 +6,9 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import random
+import argparse
+from PIL import Image
+import numpy as np
 
 import tensorflow as tf
 
@@ -22,6 +24,7 @@ TRAIN_LR_DIR = lambda s: LOCAL_DIR + 'DIV2K_train_LR_bicubic/X{}/'.format(s)
 TRAIN_HR_DIR = LOCAL_DIR + 'DIV2K_train_HR/'
 EVAL_LR_DIR = lambda s: LOCAL_DIR + 'DIV2K_valid_LR_bicubic/X{}/'.format(s)
 EVAL_HR_DIR = LOCAL_DIR + 'DIV2K_valid_HR/'
+NUM_CHANNELS = 3
 
 
 def update_argparser(parser):
@@ -34,7 +37,7 @@ def update_argparser(parser):
       default=48,
       type=int)
   parser.set_defaults(
-      num_channels=3,
+      num_channels=NUM_CHANNELS,
       train_batch_size=16,
       eval_batch_size=1,
       shuffle_buffer_size=800,
@@ -62,35 +65,12 @@ def _extract(mode, params):
   dataset = tf.data.Dataset.from_tensor_slices((lr_files, hr_files))
 
   def _read_image(lr_file, hr_file):
-    lr_image = tf.image.decode_png(tf.read_file(lr_file), channels=3)
-    hr_image = tf.image.decode_png(tf.read_file(hr_file), channels=3)
+    lr_image = tf.image.decode_png(tf.read_file(lr_file), channels=NUM_CHANNELS)
+    hr_image = tf.image.decode_png(tf.read_file(hr_file), channels=NUM_CHANNELS)
     return lr_image, hr_image
 
   dataset = dataset.map(
       _read_image,
-      num_parallel_calls=params.num_data_threads,
-  )
-  dataset = dataset.cache()
-  return dataset
-
-  def __generator(lr_dir, hr_dir):
-    for lr, hr in zip(sorted(os.listdir(lr_dir)), sorted(os.listdir(hr_dir))):
-      lr = os.path.join(lr_dir, lr)
-      hr = os.path.join(hr_dir, hr)
-      yield lr, hr
-
-  dataset = tf.data.Dataset.from_generator(
-      generator=lambda: __generator(lr_dir, hr_dir),
-      output_types=(tf.string, tf.string),
-  )
-
-  def __read_image(mode, lr, hr):
-    lr = tf.image.decode_png(tf.read_file(lr), channels=3)
-    hr = tf.image.decode_png(tf.read_file(hr), channels=3)
-    return lr, hr
-
-  dataset = dataset.map(
-      lambda *args: __read_image(mode, *args),
       num_parallel_calls=params.num_data_threads,
   )
   dataset = dataset.cache()
@@ -116,11 +96,11 @@ def _transform(dataset, mode, params):
           maxval=lr_shape[1] - params.lr_patch_size,
           dtype=tf.int32)
       lr = tf.slice(lr, [lr_up, lr_left, 0],
-                    [params.lr_patch_size, params.lr_patch_size, 3])
+                    [params.lr_patch_size, params.lr_patch_size, -1])
       hr_up = lr_up * params.scale
       hr_left = lr_left * params.scale
       hr_patch_size = params.lr_patch_size * params.scale
-      hr = tf.slice(hr, [hr_up, hr_left, 0], [hr_patch_size, hr_patch_size, 3])
+      hr = tf.slice(hr, [hr_up, hr_left, 0], [hr_patch_size, hr_patch_size, -1])
 
       def _to_be_or_not_to_be(values, fn):
 
@@ -140,8 +120,8 @@ def _transform(dataset, mode, params):
       lr, hr = _to_be_or_not_to_be([lr, hr], tf.image.flip_up_down)
       lr, hr = _to_be_or_not_to_be([lr, hr], tf.image.rot90)
 
-    lr = tf.to_float(lr)
-    hr = tf.to_float(hr)
+    lr = tf.image.convert_image_dtype(lr, tf.float32)
+    hr = tf.image.convert_image_dtype(hr, tf.float32)
 
     return {'source': lr}, {'target': hr}
 
@@ -161,40 +141,67 @@ def _transform(dataset, mode, params):
   return dataset
 
 
-def __transform(mode, lr, hr):
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    # lr_shape = tf.shape(lr)
-    # up_lr = tf.random_uniform(1, maxval=lr_shape[0] - PATCH_SIZE_LR, dtype=tf.int32)
-    # left_lr = tf.random_uniform(1, maxval=lr_shape[1] - PATCH_SIZE_LR, dtype=tf.int32)
-    # lr = tf.slice(lr, [up_lr, left_lr, 0], [PATCH_SIZE_LR, PATCH_SIZE_LR, 3])
-    def extract_patches(lr, hr):
-      up_lr = random.randrange(0, lr.shape[0] - PATCH_SIZE_LR + 1)
-      left_lr = random.randrange(0, lr.shape[1] - PATCH_SIZE_LR + 1)
-      up_hr = up_lr * SCALE
-      left_hr = left_lr * SCALE
-      lr = lr[up_lr:up_lr + PATCH_SIZE_LR, left_lr:left_lr + PATCH_SIZE_LR, :]
-      hr = hr[up_hr:up_hr + PATCH_SIZE_HR, left_hr:left_hr + PATCH_SIZE_HR, :]
-      if random.random() < 0.5:
-        lr = lr[:, ::-1, :]
-        hr = hr[:, ::-1, :]
-      if random.random() < 0.5:
-        lr = lr[::-1, :, :]
-        hr = hr[::-1, :, :]
-      if random.random() < 0.5:
-        lr = lr.transpose(1, 0, 2)
-        hr = hr.transpose(1, 0, 2)
-      return lr, hr
-
-    lr, hr = tf.py_func(
-        extract_patches, [lr, hr], [tf.uint8, tf.uint8], stateful=False)
-    lr = tf.reshape(lr, [PATCH_SIZE_LR, PATCH_SIZE_LR, 3])
-    hr = tf.reshape(hr, [PATCH_SIZE_HR, PATCH_SIZE_HR, 3])
-
-  lr = tf.to_float(lr)
-  hr = tf.to_float(hr)
-
-  return {"source": lr}, {"target": hr}
-
-
 input_fn = lambda mode, params: (
     dataset.input_fn_tplt(mode, params, extract=_extract, transform=_transform))
+
+
+def predict_input_fn():
+  serialized_tf_example = tf.placeholder(
+      dtype=tf.string, shape=[], name='input_example_tensor')
+  feature_spec = {
+      'image/encoded': tf.FixedLenFeature(shape=[], dtype=tf.string),
+  }
+  parsed_tf_example = tf.parse_single_example(serialized_tf_example,
+                                              feature_spec)
+  image_encoded = parsed_tf_example['image/encoded']
+  image = tf.image.decode_png(image_encoded, channels=NUM_CHANNELS)
+  image = tf.image.convert_image_dtype(image, tf.float32)
+  features = {'source': tf.expand_dims(image, axis=0)}
+  return tf.estimator.export.ServingInputReceiver(
+      features=features,
+      receiver_tensors={
+          tf.saved_model.signature_constants.REGRESS_INPUTS:
+              serialized_tf_example
+      })
+
+
+def test_saved_model():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--model-dir', help='GCS location to load exported model', required=True)
+  parser.add_argument(
+      '--input-dir', help='GCS location to load input images', required=True)
+  parser.add_argument(
+      '--output-dir', help='GCS location to load output images', required=True)
+  args = parser.parse_args()
+
+  with tf.Session(graph=tf.Graph()) as sess:
+    metagraph_def = tf.saved_model.loader.load(
+        sess, [tf.saved_model.tag_constants.SERVING], args.model_dir)
+    signature_def = metagraph_def.signature_def[
+        tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
+    input_tensor = sess.graph.get_tensor_by_name(
+        signature_def.inputs['inputs'].name)
+    output_tensor = sess.graph.get_tensor_by_name(
+        signature_def.outputs['outputs'].name)
+    if not os.path.isdir(args.output_dir):
+      os.mkdir(args.output_dir)
+    for input_file in os.listdir(args.input_dir):
+      print(input_file)
+      output_file = os.path.join(args.output_dir, input_file)
+      input_file = os.path.join(args.input_dir, input_file)
+      with open(input_file, 'rb') as f:
+        input_image = f.read()
+      image_encoded = tf.train.Feature(
+          bytes_list=tf.train.BytesList(value=[input_image]))
+      example = tf.train.Example(
+          features=tf.train.Features(feature={'image/encoded': image_encoded}))
+      output_image = output_tensor.eval(
+          feed_dict={input_tensor: example.SerializeToString()})
+      output_image = np.around(output_image[0] * 255.0).astype(np.uint8)
+      output_image = Image.fromarray(output_image, 'RGB')
+      output_image.save(output_file)
+
+
+if __name__ == '__main__':
+  test_saved_model()
--- model/edsr.py
+++ model/edsr.py
@@ -20,22 +20,24 @@ def update_argparser(parser):
   parser.add_argument(
       '--num-residual-units',
       help='Number of residual units in networks',
-      default=32,
+      default=64,
       type=int)
   parser.set_defaults(
       train_steps=1500000,
-      #learning_rate=((10000, 1000000, 1200000, 1400000), (1e-3, 1e-4, 5e-5,
-      #                                                    3e-5, 1e-5)),
-      learning_rate=((1000000, 1200000, 1300000, 1400000), (1e-3, 5e-4, 3e-4,
-                                                            1e-4, 5e-5)),
+      learning_rate=((1000000,), (1e-4, 5e-5)),
       save_checkpoints_steps=50000,
+      save_summary_steps=10000,
   )
 
 
 def model_fn(features, labels, mode, params, config):
+  predictions = None
+  loss = None
+  train_op = None
+  eval_metric_ops = None
+  export_outputs = None
+
   lr = features['source']
-  if mode != tf.estimator.ModeKeys.PREDICT:
-    hr = labels['target']
 
   def _edsr(x):
 
@@ -43,7 +45,7 @@ def model_fn(features, labels, mode, params, config):
       skip = x
       x = tf.layers.conv2d(
           x,
-          num_channels * 4,
+          num_channels,
           3,
           padding='same',
           name='conv0',
@@ -58,19 +60,21 @@ def model_fn(features, labels, mode, params, config):
       )
       return x + skip
 
-    def _subpixel_block(x, kernel_size):
-      x = tf.layers.conv2d_transpose(
+    def _subpixel_block(x,
+                        kernel_size,
+                        num_channels=params.num_channels,
+                        scale=params.scale):
+      x = tf.layers.conv2d(
           x,
-          params.num_channels,
-          kernel_size * params.scale,
-          params.scale,
+          num_channels * scale * scale,
+          kernel_size,
+          padding='same',
       )
-      boundary_size = (kernel_size - 1) * params.scale
-      x = x[:, boundary_size:-boundary_size, boundary_size:-boundary_size, :]
+      x = tf.depth_to_space(x, scale)
       return x
 
     MEAN = tf.constant([0.4488, 0.4371, 0.4040], dtype=tf.float32)
-    x = (x / 255.0) - MEAN
+    x = x - MEAN
     with tf.variable_scope('skip'):
       skip = _subpixel_block(x, 5)
     with tf.variable_scope('input'):
@@ -78,67 +82,67 @@ def model_fn(features, labels, mode, params, config):
           x,
           params.num_residual_units,
           3,
-          padding='valid',
+          padding='same',
       )
     for i in range(params.num_blocks):
       with tf.variable_scope('layer{}'.format(i)):
         x = _residual_block(x, params.num_residual_units)
     with tf.variable_scope('output'):
       x = _subpixel_block(x, 3)
-    x = x + skip
-    x = (x + MEAN) * 255.0
+    x += skip
+    x = x + MEAN
     return x
-
-  global_step = tf.train.get_or_create_global_step()
-
-  boundary_size_lr = 2
-  boundary_size_hr = boundary_size_lr * params.scale
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    hr = hr[:, boundary_size_hr:-boundary_size_hr, boundary_size_hr:
-            -boundary_size_hr, :]
-  else:
-    lr = tf.pad(lr, [[0, 0], [boundary_size_lr, boundary_size_lr],
-                     [boundary_size_lr, boundary_size_lr], [0, 0]], 'SYMMETRIC')
 
   sr = _edsr(lr)
 
-  predictions = tf.clip_by_value(tf.round(sr), 0.0, 255.0)
+  predictions = tf.clip_by_value(sr, 0.0, 1.0)
 
-  if mode != tf.estimator.ModeKeys.PREDICT:
-    loss = tf.losses.absolute_difference(labels=hr, predictions=sr)
-  else:
-    loss = None
-
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    learning_rate = tf.train.piecewise_constant(
-        global_step, params.learning_rate[0], params.learning_rate[1])
-    opt = tf.train.AdamOptimizer(learning_rate)
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    with tf.control_dependencies(update_ops):
-      train_op = opt.minimize(loss, global_step=global_step)
-  else:
-    train_op = None
-
-  if mode == tf.estimator.ModeKeys.EVAL:
-    boundary_size_mse = params.scale + 6
-    psnr = tf.image.psnr(
-        hr[:, boundary_size_mse:-boundary_size_mse, boundary_size_mse:
-           -boundary_size_mse, :],
-        predictions[:, boundary_size_mse:-boundary_size_mse, boundary_size_mse:
-                    -boundary_size_mse, :],
-        max_val=255.0,
-    )
-    eval_metrics = {
-        'PSNR': tf.metrics.mean(psnr),
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    export_outputs = {
+        tf.saved_model.signature_constants.REGRESS_METHOD_NAME:
+            tf.estimator.export.RegressionOutput(predictions)
     }
   else:
-    eval_metrics = None
+    hr = labels['target']
+    loss = tf.losses.absolute_difference(labels=hr, predictions=sr)
+    if mode == tf.estimator.ModeKeys.EVAL:
+
+      def _ignore_boundary(images):
+        boundary_size = params.scale + 6
+        images = images[:, boundary_size:-boundary_size, boundary_size:
+                        -boundary_size, :]
+        return images
+
+      def _float32_to_uint8(images):
+        images = images * 255.0
+        images = tf.round(images)
+        images = tf.saturate_cast(images, tf.uint8)
+        return images
+
+      psnr = tf.image.psnr(
+          _float32_to_uint8(_ignore_boundary(hr)),
+          _float32_to_uint8(_ignore_boundary(predictions)),
+          max_val=255,
+      )
+      eval_metric_ops = {
+          'PSNR': tf.metrics.mean(psnr),
+      }
+    if mode == tf.estimator.ModeKeys.TRAIN:
+      global_step = tf.train.get_or_create_global_step()
+      learning_rate = tf.train.piecewise_constant(
+          global_step, params.learning_rate[0], params.learning_rate[1])
+      opt = tf.train.AdamOptimizer(learning_rate)
+      update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+      with tf.control_dependencies(update_ops):
+        train_op = opt.minimize(loss, global_step=global_step)
+      stats = tf.profiler.profile()
+      print("Total parameters:", stats.total_parameters)
 
   return tf.estimator.EstimatorSpec(
       mode=mode,
       predictions=predictions,
       loss=loss,
       train_op=train_op,
-      eval_metric_ops=eval_metrics,
-      export_outputs=None,
+      eval_metric_ops=eval_metric_ops,
+      export_outputs=export_outputs,
   )
